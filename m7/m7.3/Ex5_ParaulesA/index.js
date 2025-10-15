0e82e3b6f48f3f66007f52b6f1a12691cb940660
Irene Virgínia <isahun7@gmail.com>
2025-10-15 19:13:26 +0200

    ex5 resolt

--- m7/m7.3/Ex5_ParaulesA/index.js
+++ m7/m7.3/Ex5_ParaulesA/index.js
@@ -1,48 +1,44 @@
 "use strict"
 
-/*<!--Donat un array de paraules introduïdes per l’usuari/ària. Digues el nombre de paraules que contenen la lletra ‘a’ i mostra-les per pantalla.
--->
-<p>Introdueix paraules:</p>
-<input type="text" placeholder="Introdueix una paraula" id="wordUser">
-<p></p>
-<button type="button" onclick="wordsWithA()">Fes clic</button>
-<p></p>
-<div id="result"></div>
-<script src="index.js"></script>*/
 
-function validateWord(userWord) {
-    return userWord.trim() == "" || !isNaN(userWord) 
-}
-
-function namesArray () {
-        const userWord = document.getElementById("userWord").value;
-        const userWordArr = [];
-
-        userWordArr.push(userWord);
-}
-
-/*function wordsWithA() {
-    const userWord = document.getElementById("userWord").value;
+function wordsWithA () {
+    let numWords = parseInt(prompt("Quantes paraules vols introduir a la llista?")); //recollir dades amb prompt no input
+    const wordsArr = [];
+    let userWord = "";
     const resultDiv = document.getElementById("result");
-    const userWordArr = [];
-    const userAWordArr = [];
-    
-    const err = "Has d'introduir una paraula."
 
-    let message = "";
 
-if (validateWord(userWord)) return resultDiv.innerHTML = err;
-
-let firstChar = userWord.charAt(0).toLowerCase();
-
-userWordArr.push(userWord);
-
-if (firstChar == "a") {
-userAWordArr.push(userWord);
+//validacions
+while (numWords <= 0 || isNaN(numWords)) { //validem amb bucle while per a que mostri error mentre el num sigui 0 o menys, o sigui text
+    alert("Introdueix un número major a 0");
+    numWords = parseInt(prompt("Quantes paraules vols introduir a la llista?")); //tornem a declarar la variable per a que pugui entrar al for seguent, 
+    //on es demana la paraula a introduir i s'incorpora a l'array
 }
 
-resultDiv.innerHTML = `Hi ha ${userAWordArr.length} paraules amb A a la teva llista: ${userAWordArr.join("<br>")}`
+//BUCLE FOR
+for (let i = 0; i < numWords; i++) { //primer bucle for per recórrer tot el nou array a mida que es vagi formant amb nous inputs gracies al codi de dins
+    userWord = prompt("Quines paraules vols introduir a la llista?"); //recollim input string de user
+    wordsArr.push(userWord); //introduim aquest valor a l'array amb .push()
+}
 
-}*/
+console.log(userWord); //per comprovar que l'array conté tots els inputs que posa l'usuari
 
+const nomsAmbA = []; //creem un nou array fora del bucle, on guardarem els valors q comencen per A del primer array, si n'hi ha
 
+for (let i = 0; i < wordsArr.length; i++) { //tornem a recórrer l'array amb aquest bucle for, aquet cop x filtrar valors q comencen x A
+
+    let firstChar = wordsArr[i].toLowerCase().charAt(0); //tb nomsLlista[i][0].toLowerCase(). No podem fer .toLowerCase().charAt() a un array, només a un string
+    //amb aquesta variable firstChar fem possible comparar el primer caràcter de cada valor de l'array original
+
+    if (firstChar == "a") {
+        nomsAmbA.push(wordsArr[i]); 
+}
+}
+
+if (nomsAmbA.length > 0) { 
+    resultDiv.innerHTML = `La teva llista té ${nomsAmbA.length} noms començats per A: </br> ${nomsAmbA.join("<br>")}`;
+} else {
+    resultDiv.innerHTML = `La teva llista no inclou cap element començat per A.`;
+}
+
+}
