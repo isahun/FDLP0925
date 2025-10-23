9b1ecc177d37164a305549d6062d7241ecad7a7c
Irene Virgínia <isahun7@gmail.com>
2025-10-23 20:55:31 +0200

    a per l'ultim del m7

---
+++ m7/m7.3/Ex7_surf/index.js
@@ -0,0 +1,22 @@
+"use strict"
+
+/*
+Escriu un programa per una botiga de surf on se li exposarà un menú als usuaris i usuàries amb les següents opcions:
+
+Calcular material taula (crida a una funció que rep un boolean que representi si la persona té experiència o no) si té, 
+retornarà el missatge "taula de fibra", si no retornarà el missatge "taula d'espuma". (demanem per pantalla si l'usuari/ària si té experiència o no)
+
+Imprimir forecast (crida a una funció  que rep l'alçada de l'onada en metres). Si l'alçada és més de 2 metres, imprimirà "onades grans", 
+si no imprimirà "onades petites". (Introduïm l'alçada de les onades per pantalla, simulant que les rebem d'una aplicació externa)
+
+Calcula el cost per reserva (crida a una funció  que rep els minuts de reserva i el tipus de taula reservada i retorna el cost de la reserva).
+(demanem per pantalla els minuts i el tipus)
+
+Taula tipus espuma: 20 € cada 60 min.
+Taula tipus fibra: 35 € cada 60 min.
+Sortir del programa.
+
+Fins que la persona no escrigui un 4, el programa ha de continuar mostrant el menú i donar la possibilitat de fer accions.
+-->
+*/
+
