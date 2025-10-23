9f6f7846581152247c18bb3535ba7d5b4c289c47
Irene <isahun7@gmail.com>
2025-10-23 17:59:22 +0200

    avançant amb ex2

--- M8/Ex2/Vol.js
+++ M8/Ex2/Vol.js
@@ -2,7 +2,13 @@
 class Vol {
 
     #flightNum; // lunic q necessita privacitat really
-    
+//statics (una altra tipus de propietat, com public, privada)
+
+static km = 1000;//PROPIETATS ESTÀTIQUES, les definim a la classe, 
+// no al constructor, aixi hem d copiar menys cops i gastem menys espai en memòria
+static tons = 0.1;
+static count = 1;
+
 //1. Constructor
 
 constructor (flightNum, distanceKm, company, numPassengers, numMotors) {
@@ -11,8 +17,15 @@ constructor (flightNum, distanceKm, company, numPassengers, numMotors) {
     this._company = company;
     this._numPassengers = numPassengers;
     this._numMotors = numMotors;
-    this.km = 1000; //declarem aqui la constant dels km x tnes de carboni
-    this.tons = 0.1 //declarem aquí la constant de les tones de carboni x la funcio, pro en breus ho treurem
+    this.id = Vol.count++ //metode de classe directament pk es static, de classe
+    //this.id serà l'identificador de la instància, el comptador cada cop q es creï una
+
+    //El count va incrementant, com q pertany a la classe no es perd, 
+    //i abans d’incrementar presta el seu valor al canto esquerre
+
+    //Si enlloc de this.id = Vol.count ++ fos this.id = ++Vol.count, hauriem de començar el count a 0, pk primer incrementa
+    //this.km = 1000; //declarem aqui la constant dels km x tnes de carboni
+    //this.tons = 0.1 //declarem aquí la constant de les tones de carboni x la funcio, pro en breus ho treurem
 }
 
 //2. Getters i Setters
@@ -24,19 +37,22 @@ get numPassengers() { return this._numPassengers }
 get numMotors() { return this._numMotors }
 
 //setters
-
-set flightNum(newFlightNum) { this.#flightNum = newFlightNum }
+set flightNum(newFlightNum) { this.#flightNum = newFlightNum } //es igual el nom q posem aqui, importa l'ordre d'arribada, entrelazado cuantico
 set distanceKm(newDistanceKm) { this._distanceKm = newDistanceKm }
 set company(newCompany) { this._company = newCompany }
 set numPassengers(newNumPassengers) { this._numPassengers = newNumPassengers }
 set numMotors(newNumMotors) { this._numMotors = newNumMotors }
 
-calcCarbonFootprint () { return ((this._distanceKm / this.km) * this.tons) * this._numMotors }
+calcCarbonFootprint () { 
+    return ((this._distanceKm / Vol.km) * Vol.tons) * this.numMotors  
+    //les propietats estatiques citen directament la classe enlloc del this (k es exclusiu del contingu del constructor)
+}
 
 toString() { //str, no posem ni _ ni # pk ja fem servir getters
         return `
             Número de vol: ${this.flightNum} 
             Distància (km): ${this.distanceKm}
+            Companyia aèria: ${this.company}
             Número de passatgers: ${this.numPassengers}
             Número motors: ${this.numMotors}
             Petjada de carboni: ${this.calcCarbonFootprint()}
--- M8/Ex2/index.js
+++ M8/Ex2/index.js
@@ -1,5 +1,25 @@
 "use strict"
 
+
+const resultDiv = document.getElementById("result");
+
+
+function findFlight(nVol) { //Ens tornarà una posició, un NUMERO. Entrelazado cuantico amb el num d vol q introduirà l'usuari
+
+    //flightNum q busquem, dins flightsArr
+    let i = 0; //compte
+    let position = -1 //partim de que no ho troba
+
+    do {
+        if(flightsArr[i].flightNum === nVol) {
+            position = i;
+        }
+        i++
+    } while (i < flightsArr.length && position == -1)
+
+    return position; //la funció genera un nombre, l'index
+}
+
 function createFlight() {
 
 //const client1 = new Client ("26374857J", "40", "Sueca", 6);
@@ -10,11 +30,17 @@ const distanceKm = parseInt(document.getElementById("distanceKm").value);
 const flightCompany = document.getElementById("company").value;
 const numPassengers = document.getElementById("numPassengers").value;
 const numMotors = parseInt(document.getElementById("numMotors").value);
-const resultDiv = document.getElementById("result");
-const err = '<small style="color:RedEyeReduction;"> Invalid values </small>';
 
-if (!flightNum || !distanceKm || !numPassengers || !numMotors) return resultDiv.innerHTML = err;
+const err = '<small style="color:RedEyeReduction;"> Invalid values </small>';
+const err2 = '<small style="color:RedEyeReduction;"> El vol ja existeix </small>';
+
+
+if (!flightNum || !distanceKm || !flightCompany || !numPassengers || !numMotors) return resultDiv.innerHTML = err;
 //podem fer !variable x les numeriques tambe
+
+//comprovem si l'array no conté ja un element igual amb la funcio creada abans
+if (findFlight(flightNum) !== -1 ) return resultDiv.innerHTML = err2;
+//pedazo de validacio, ens hem assegurat que si ja existeix no es pugui crear, pero si no, si 
 
 //instància
 const newFlight = new Vol (flightNum, distanceKm, flightCompany, numPassengers, numMotors);
@@ -25,11 +51,41 @@ resultDiv.innerHTML = `<pre> ${newFlight.toString()} </pre>`;
 
 }
 
+function readAllFlights() {
+    //recorrem array, i aquet cop el necessitem sencer, aixi q bucle FOR
+    if (flightsArr.length < 1) return resultDiv.innerHTML = "No hi ha cap vol creat."
+    let message = "";
+    //bucle x recorrer cada element:
+    for (let i = 0; i < flightsArr.length; i++) {
+        const currentFlight = flightsArr[i];
+        message += `<pre> ${currentFlight.toString()} </pre><br>`
+    }
+//un cop ja tenim tot el toString de tots els elements acumulat a la variable message, 
+// com q hem tret resultdiv fora com a global: 
+
+resultDiv.innerHTML = message;
+
+}
+
+function checkFlight() {
+    if (flightsArr.length < 1) return resultDiv.innerHTML = "No hi ha vols guardats."
+    
+    //recollim input i validem
+    const flightReadUser = document.getElementById("flightRead").value;
+    if (!flightReadUser) return resultDiv.innerHTML = "Has d'introduir un número de vol vàlid."
+
+    //buscar al array amb la funció
+    const flightPosition = findFlight(flightReadUser)
+    if (flightPosition == -1) return resultDiv.innerHTML = "Aquest vol no existeix.";
+
+    resultDiv.innerHTML = flightsArr[flightPosition].toString();
+}
+
 function removeFlight () {
 
 //Aquesta funció busca dins de l’array clients un objecte Vol que tingui un DNI igual al que l’usuari escriu per prompt(). 
 //comprovem si l'array és buit
-    if (flightsArr.length < 1) return document.getElementById("result").innerHTML = "No hi ha vols guardats."
+    if (flightsArr.length < 1) return resultDiv.innerHTML = "No hi ha vols guardats."
 
     const flightFilter = prompt("Escrigui el seu número de vol");
 
@@ -50,43 +106,15 @@ function removeFlight () {
 
         if (position != -1) {
             flightsArr.splice(i, 1);
-            return document.getElementById("result").innerHTML = "S'ha eliminat el vol correctament."
+            return resultDiv.innerHTML = "S'ha eliminat el vol correctament."
         } else {
-            return document.getElementById("result").innerHTML = "No existeix aquest vol en la teva base de dades";
-        } //Missatges diferents si s’ha trobat o no
-
-}
-
-function checkFlight() {
-    if (flightsArr.length < 1) return document.getElementById("result").innerHTML = "No hi ha vols guardats."
-
-    const flightFilter = prompt("Escrigui el seu número de vol");
-
-    let i = 0; //comptador d bucle, posicio actual dins array
-    let position = -1; //aqui guardarem la posicio on trobem el client
-
-    do { //bucle x recorrer tot l'array, client x client 
-        //SI VOLEM Q ENS MOSTRI TOTS ELS ELEMENTS, FEM UN BUCLE FOR, PK VOLEM LLELEGIR TOTES LES POSICIONS DE L'ARRAY
-
-        if (flightsArr[i].flightNum == flightFilter){
-            position = i; //si hi ha coincidencia, actualitzem la variable de posició
-        }
-
-        i++ //augmentem i per passar al seguent client, i continua fins arribar al final de l'array
-    } while ( i < flightsArr.length && position == -1) //fins que la i sigui mes gran q el maxim d posicions d l'array o no es trobi
-    //si mostra -1 vol dir q no hi ha cap client amb aquet DNI
-        console.log(position, "posició"); //ens torna la posició
-
-        if (position != -1) {
-            return document.getElementById("result").innerHTML = toString()
-        } else {
-            return document.getElementById("result").innerHTML = "No existeix aquest vol en la teva base de dades";
+            return resultDiv.innerHTML = "No existeix aquest vol en la teva base de dades";
         } //Missatges diferents si s’ha trobat o no
 
 }
 
 function changeFlight() {
-    if (flightsArr.length < 1) return document.getElementById("result").innerHTML = "No hi ha vols guardats amb aquest número."
+    if (flightsArr.length < 1) return resultDiv.innerHTML = "No hi ha vols guardats amb aquest número."
 
     const flightFilter = prompt("Escrigui el seu número de vol");
 
